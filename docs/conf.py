05f3f02558e891b88bde8eacfdf9f7d1bfca3bb7
Antoine Capeyron <73116452+acapeyron@users.noreply.github.com>
2022-03-14 12:03:33 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -33,10 +33,6 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinxcontrib.jupyter',
-    'sphinxcontrib.bibtex',
-]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['templates']
